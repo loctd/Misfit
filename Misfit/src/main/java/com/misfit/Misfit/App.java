a48808fd6024d545734342ee903451453c1913a4
Loc Tran <loctran@fossil.com>
2016-07-15 10:07:07 +0700

    Add constants

---
+++ Misfit/src/main/java/com/misfit/Misfit/App.java
@@ -0,0 +1,13 @@
+package com.misfit.Misfit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Misfit/src/main/java/com/misfit/WebConstants/Constants.java
@@ -0,0 +1,54 @@
+package com.misfit.WebConstants;
+
+public class Constants {
+
+	//Footer products
+	public static final String XPATH_RAY = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[1]/div/div/ul/li[1]/a";
+	public static final String XPATH_SHINE2 = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[1]/div/div/ul/li[2]/a";
+	public static final String XPATH_SPEEDO_SHINE2 = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[1]/div/div/ul/li[3]/a";
+	public static final String XPATH_SHINE = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[1]/div/div/ul/li[4]/a";
+	public static final String XPATH_SPEEDO_SHINE = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[1]/div/div/ul/li[5]/a";
+	public static final String XPATH_SWAROVSKI = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[1]/div/div/ul/li[6]/a";
+	public static final String XPATH_FLASH = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[1]/div/div/ul/li[7]/a";
+	public static final String XPATH_LINK = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[1]/div/div/ul/li[8]/a";
+	public static final String XPATH_GEAR = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[1]/div/div/ul/li[9]/a";
+	public static final String XPATH_WHERETOBUY = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[1]/div/div/ul/li[10]/a";
+	
+	//Footer Misfit app
+	public static final String XPATH_MISFIT_APP = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[2]/div[1]/div/ul/li[1]/a";
+	public static final String XPATH_HOME_APP = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[2]/div[1]/div/ul/li[2]/a";
+	public static final String XPATH_LINK_APP = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[2]/div[1]/div/ul/li[3]/a";
+	public static final String XPATH_CYCLING_APP = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[2]/div[1]/div/ul/li[4]/a";
+	
+	//Footer Work With Misfit
+	public static final String XPATH_HEALTH = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[2]/div[2]/div/ul/li[1]/a";
+	public static final String XPATH_AFFILIATE = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[2]/div[2]/div/ul/li[2]/a";
+	public static final String XPATH_DEVELOPER = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[2]/div[2]/div/ul/li[3]/a";
+	
+	//Footer About
+	public static final String XPATH_ABOUT_MISFIT = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[3]/div[1]/div/ul/li[1]/a";
+	public static final String XPATH_ABOUT_JOBS = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[3]/div[1]/div/ul/li[2]/a";
+	
+	//Footer Support
+	public static final String XPATH_SUPPORT_PAGE = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[3]/div[2]/div/ul/li[1]/a";
+	public static final String XPATH_SUPPORT_GETTINGSTATED = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[3]/div[2]/div/ul/li[2]/a";
+	public static final String XPATH_SUPPORT_CONTACT = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[3]/div[2]/div/ul/li[3]/a";
+	
+	//Footer Media
+	public static final String XPATH_MEDIA_ANNOUNCEMENTS = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[3]/div[3]/div/ul/li[1]/a";
+	public static final String XPATH_MEDIA_BLOGS = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[3]/div[3]/div/ul/li[2]/a";
+	public static final String XPATH_MEDIA_MARKETING = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[3]/div[3]/div/ul/li[3]/a";
+			
+	//Footer Legal
+	public static final String XPATH_LEGAL_TERM = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[4]/div[1]/div/ul/li[1]/a";
+	public static final String XPATH_LEGAL_PRIVACY = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[4]/div[1]/div/ul/li[2]/a";
+	public static final String XPATH_LEGAL_WARRANTY = ".//*[@id='footer']/div/nav/section/div[2]/div[1]/div/div[4]/div[1]/div/ul/li[3]/a";
+	
+	//Footer Social
+	public static final String XPATH_SOCIAL_FACEBOOK = "";
+	public static final String XPATH_SOCIAL_TWEETER = "";
+	public static final String XPATH_SOCIAL_INSTAGRAM = "";
+	public static final String XPATH_SOCIAL_YOUTUBE = "";
+	public static final String XPATH_SOCIAL_GOOGLE = "";
+	public static final String XPATH_SOCIAL_PINTEREST = "";
+}
